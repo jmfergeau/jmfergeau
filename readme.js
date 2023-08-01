569580ef78143f2ef142c85989e5cd35a291964f
maxlefou <buttbadger@gmail.com>
2023-08-01 11:40:28 +0200

    testing that gabot

---
+++ readme.js
@@ -0,0 +1,29 @@
+module.exports = `### Hi there, I'm Julien-Maxime, or Max! 👋
+
+<img src="https://github.com/maxlefou/maxlefou/raw/master/avatar.png" align="right" height="150px">
+I'm a passionate french self-learning frontend webdev actually looking for a job. I'm also a software translator french-english, an opensource lover and I develop videogames as a hobby.
+
+Note that most of my activity is on [Gitlab](https://gitlab.com/maxlefou) and that I only share here my public/opensource works which are actually Gitlab mirror repositories.
+
+I'm in an urgent need of a job, so please consider checking [📓 My portfolio](https://jmf-portfolio.netlify.com/)!
+
+<p align="center"><i>Thanks for stopping by!</i> 🙏</p>
+
+<!--Statistics-->
+<a href="https://github.com/jmfergeau">
+  <img height="180em" src="https://github-readme-stats.vercel.app/api?username=jmfergeau&theme=dark&show_icons=true&count_private=true" />
+  <img height="180em" src="https://github-readme-stats.vercel.app/api/top-langs/?username=jmfergeau&theme=dark&layout=compact&count_private=true" />
+</a>
+
+### Visitor count
+
+<img src="https://profile-counter.glitch.me/Melvynx/count.svg" />
+
+Added the \`Mon 3 Jan 2022\`
+
+Last update on <#today_date>
+
+<#day_before_new_years> days before new years
+
+<#gabot_signing>
+`;
